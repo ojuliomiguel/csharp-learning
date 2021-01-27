3d096be3bfb9a336f7db2673dbff2bfd5fec3d36
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-01-26 22:52:01 -0300

    Aprendendo sobre o razor

---
+++ aspnet/RazorPagesMovie/Migrations/20210126215750_FixTitle.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class FixTitle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Titile",
+                table: "Movie",
+                newName: "Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Movie",
+                newName: "Titile");
+        }
+    }
+}
