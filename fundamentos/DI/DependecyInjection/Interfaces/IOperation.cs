789a696ac68f5b0eb4e4c7356c34a3944e329800
ojuliomiguel <juliomiguelsouzacosta@gmail.com>
2021-02-11 10:10:28 -0300

    adding interfaces baseadas em IOperation

---
+++ fundamentos/DI/DependecyInjection/Interfaces/IOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependecyInjection.Interfaces
+{
+    public interface IOperation
+    {
+        public Guid IOperation { get;}
+    }
+
+    public interface IOperationTransient: IOperation
+    {
+
+    }
+
+    public interface IOperationScoped : IOperation
+    {
+
+    }
+
+    public interface IOperationSingleton : IOperation
+    {
+
+    }
+
+    public interface IOperationSingletonInstace : IOperation
+    {
+
+    }
+}
